1c8eb1042c39de2688b25c3715d2e071e0ad4d53
Noah Abrams <noah.jean.abrams@gmail.com>
2022-04-07 15:59:52 +0200

    fertige version von Wahr oder Falsch minispiel

--- frontend/QaA/main.js
+++ frontend/QaA/main.js
@@ -1,81 +1,117 @@
-var x;
-var y =3;
+
 var min = 1;
 var max = 12;
  
 
-
-
-
+window.addEventListener("load", function() {
+    zufall();
+})
 
 function zufall(){
-    x = (Math.random() * (max - min)) + min;
-    y = Math.round(x)
-    document.getElementById("question").innerHTML = q;
-    swichquest();
-
-}
-
-
-
-
-function swichquest(){
+    let x = (Math.random() * (max - min)) + min;
+    let y = Math.round(x)
     if (y==1){
-        q="was ist 1+1"
+        q="Das Spiel funktionirt"
         a=true
     }
     if (y==2){
-        q="aws ist 2+2"
+        q="Wir sind gerade in Afrika"
         a=false
     }
     if (y==3){
-        q="aws ist 3+3"
+        q="Bash ist eine Sprach"
         a=true
     }
     
     if (y==4){
-        q="aws ist 4+4"
+        q="100/5 sind 25"
         a=false
     }
     if (y==5){
-        q="aws ist 5+5"
+        q="Deutschland ist ein Land"
         a=true
     }
     if (y==6){
-        q="aws ist 6+6"
+        q="Es war noch nie jemand auf dem Mond"
         a=false
     }
     if (y==7){
-        q="aws ist 7+7"
+        q="Mir fallen keine Aussage mehr ein"
         a=true
     }
     if (y==8){
-        q="aws ist 8+8"
+        q="Unter Wasser kann man Artmen"
         a=false
     }
     if (y==9){
-        q="aws ist 9+9"
+        q="100 / 10 sind 10"
         a=true
     }
     if (y==10){
-        q="aws ist 10+10"
+        q="Du wirst die Frage richtig beantworten "
         a=true
     }
     if (y==11){
-        q="aws ist 11+11"
+        q="Das Spiel funktionirt nicht"
         a=false
     }
+    document.getElementById("question").innerHTML = q;
 }
-object.addEventListener("click", truee);
-object.addEventListener("click", falsee);
+
+
+
+
+
+
+
+
+document.getElementById("t").addEventListener("click", truee);
+document.getElementById("f").addEventListener("click", falsee);
+
 
 function truee(){
-    console.log("true")
+    if (a==true){
+        console.log("richtig")
+        animationtrue();
+    }
+    if (a==false){
+        console.log("falsch")
+        animationfalse();
+    }
+    
 }
 
 function falsee(){
-    console.log("false")
+    if (a==false){
+        console.log("richtig")
+        animationtrue();
+    }
+    if (a==true){
+        console.log("falsch")
+        animationfalse();
+    }
 }
 
 
+function animationtrue(){
+    document.getElementById("question").className = "grün";
+    document.getElementById("question").innerHTML = "Das war RICHTIG";
+    timeoutID = window.setTimeout(normaltrue, 1000);
+    
+}
+function normaltrue(){
+    document.getElementById("question").className = "normal"; 
+    zufall();
+}
 
+
+function animationfalse(){
+    document.getElementById("question").className = "rot";
+    document.getElementById("question").innerHTML = "Das war FALSCH";
+    timeoutID = window.setTimeout(normaltrue, 1000);
+   
+}
+function normalfolse(){
+    document.getElementById("question").className = "normal"; 
+    zufall();
+}
